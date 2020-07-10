616aa0316601b77238c90edeacd51e8e7c7f818a
Dimitar Grozdanov <dimitar.grozdanov@statix.company>
2020-07-10 17:04:58 +0300

    Bluetooth integration completed

---
+++ playgroundv6/CardPaymentResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playgroundv6
+{
+    public class CardPaymentResult
+    {
+        public bool TransacationCompleted { get; set; }
+        
+        public string PaymentStatus { get; set; }
+
+        public string TransactionCode { get; set; }
+
+    }
+}
--- playgroundv6/Program.cs
+++ playgroundv6/Program.cs
@@ -1,6 +1,7 @@
 ﻿using SumUp.Sdk;
 using SumUp.Sdk.Api.Emv;
 using SumUp.Sdk.Api.TransactionGateway.Dto;
+using SumUp.Sdk.Device.Facade.Dto;
 using SumUp.Sdk.Device.Reader;
 using System;
 using System.Collections.Generic;
@@ -15,7 +16,14 @@ namespace playgroundv6
 {
     public class Program
     {
+        static string message = String.Empty;
+
         static void Main(string[] args)
+        {
+            PayPos();
+        }
+
+        public static CardPaymentResult PayPos()
         {
             try
             {
@@ -27,15 +35,15 @@ namespace playgroundv6
                                 "cf768b340a13846d51bbd98fb54bcc163dca7d030a77d0fe383337e096209689", "dev_statix@sumup.com",
                                 "extdev"))).Wait();
 
-                var usbReaders = Task.Run(() => sumUpService.CardReader.FindAllUsbAsync());
-                usbReaders.Wait();
-                var usbDevicesList = usbReaders.Result;
+                //First bluetooth finding because of exception
 
-                //var findBlueToothReaders = Task.Run(() =>
-                //       sumUpService.CardReader.FindAllBluetoothGattAsync(CardReaderType.Air, 5, 10));
-                //findBlueToothReaders.Wait();
-                //var readers = findBlueToothReaders.Result;
-                //if (readers.Count == 0)
+                var findBlueToothReaders = Task.Run(() =>
+                       sumUpService.CardReader.FindAllBluetoothGattAsync(CardReaderType.AirLite, 5, 10));
+                findBlueToothReaders.Wait();
+                var blueToothReaders = findBlueToothReaders.Result;
+
+                //SKIP step below because if no bluetooth is found , find USB alternative. If no USB is found , an exception will be thrown.
+                //if (blueToothReaders.Count == 0)
                 //{
                 //    Console.WriteLine("Няма терминал", "Не е намерен POS терминал свързан с този компютър");
                 //    //Dispatcher?.Invoke(async () =>
@@ -53,23 +61,24 @@ namespace playgroundv6
                     {
                         if (paymentFinishing.Result.Message == "successful_receipt")
                         {
-                        //Set OrderPayments values
-                        //payment.Complete = true;
-                        //payment.AccountingConfirmed = true;
-                        //payment.Save();
-                        //Dispatcher?.Invoke(() => SavePayment(sender, e));
-                    }
+                            //this is executed on the web app
+                            //Set OrderPayments values
+                            //payment.Complete = true;
+                            //payment.AccountingConfirmed = true;
+                            //payment.Save();
+                            //Dispatcher?.Invoke(() => SavePayment(sender, e));
+                        }
                         else
                         {
                             Console.WriteLine();
+
                         }
                     },
                             (s, readerNotification) => { },
                             (s, statusChanged) =>
                             {
-                            //Dispatcher?.Invoke(() =>
-                            //{
-                            string message = String.Empty;
+                                //Dispatcher?.Invoke(() =>
+                                //{
                                 Console.WriteLine(statusChanged.StatusMessage);
                                 switch (statusChanged.StatusMessage)
                                 {
@@ -93,38 +102,94 @@ namespace playgroundv6
                                         break;
                                 }
 
-                            //c.SetMessage(message);
-                            Console.WriteLine(message);
-                            /*}*/
+                                //c.SetMessage(message);
+                                Console.WriteLine(message);
+                                /*}*/
                             });
 
-                //if (usbReaders.Result.Count > 0)
-                //{
-                var paymentsTask = Task.Run(() => sumUpService.CardPayment.MakePaymentUsingDefaultUsbReaderAsync(
-                    new PaymentSignatureCallback(), CancellationToken.None, new CheckoutRequest()
-                    {
-                        Amount = 1,
-                        Currency = "BGN",
-                        StartTime = DateTimeOffset.Now,
-                        TransactionId = Guid.NewGuid().ToString(),
-                        VatAmount = 0.20,
-                        CustomItems = new List<CustomItem>()
-                        {
-                                        new CustomItem()
-                                        {
-                                            Amount = 2,
-                                            Quantity = 1,
-                                            Title = "TestPayment"
-                                        }
-                        }
-                    }));
 
-                var z = paymentsTask.Result;
-                //}
+                if (blueToothReaders.Count > 0)
+                {
+                    var bluetoothPayment = Task.Run(() => sumUpService.CardPayment.MakePaymentUsingBluetoothReaderAsync(
+                             new CardReaderInformation()
+                             {
+                                 CardReaderType = CardReaderType.Air,
+                                 Id = blueToothReaders[0].Id,
+                                 Name = blueToothReaders[0].Name
+                             }, new PaymentSignatureCallback(), CancellationToken.None, new CheckoutRequest()
+                             {
+                                 Amount = 1,
+                                 Currency = "BGN",
+                                 StartTime = DateTimeOffset.Now,
+                                 TransactionId = Guid.NewGuid().ToString(),
+                                 VatAmount = 0.20,
+                                 CustomItems = new List<CustomItem>()
+                                 {
+                                    new CustomItem()
+                                    {
+                                        Amount = 1,
+                                        Quantity = 1,
+                                        Title = "Bluetooth Payment"
+                                    }
+                                 }
+                             }));
+
+                    bluetoothPayment.Wait();
+
+                    var bluetoothPaymentInfo = bluetoothPayment.Result;
+
+                    var result = new CardPaymentResult { TransacationCompleted = bluetoothPaymentInfo.IsStop, PaymentStatus = bluetoothPaymentInfo.ProcessingMessage, TransactionCode = bluetoothPaymentInfo.TransactionCode};
+
+                    return result;
+                }
+                else
+                {
+                    //NEED to use USB readers first as it the most common case.
+                    var usbReaders = Task.Run(() => sumUpService.CardReader.FindAllUsbAsync());
+                    usbReaders.Wait();
+                    var usbDevicesList = usbReaders.Result;
+
+                    if (usbReaders.Result.Count > 0)
+                    {
+                        var paymentsTask = Task.Run(() => sumUpService.CardPayment.MakePaymentUsingDefaultUsbReaderAsync(
+                            new PaymentSignatureCallback(), CancellationToken.None, new CheckoutRequest()
+                            {
+                                Amount = 1,
+                                Currency = "BGN",
+                                StartTime = DateTimeOffset.Now,
+                                TransactionId = Guid.NewGuid().ToString(),
+                                VatAmount = 0.20,
+                                CustomItems = new List<CustomItem>()
+                                {
+                                            new CustomItem()
+                                            {
+                                                Amount = 2,
+                                                Quantity = 1,
+                                                Title = "TestPayment"
+                                            }
+                                }
+                            }));
+
+                        paymentsTask.Wait();
+
+                        var usbPaymentInfo = paymentsTask.Result;
+
+                        var result = new CardPaymentResult { TransacationCompleted = usbPaymentInfo.IsStop, PaymentStatus = usbPaymentInfo.ProcessingMessage, TransactionCode = usbPaymentInfo.TransactionCode };
+
+                        return result;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Няма терминал", "Не е намерен POS терминал свързан с този компютър");
+                        return new CardPaymentResult { TransacationCompleted = false, PaymentStatus = "Не е намерен POS терминал свързан с този компютър", TransactionCode = null }; 
+                    }
+                }
 
             }
             catch (Exception ex)
             {
+                var result = new CardPaymentResult { TransacationCompleted = false, PaymentStatus = "Грешка при комуникация с POS устройството !", TransactionCode = null };
+
                 var sb = new StringBuilder();
                 sb.AppendLine(DateTime.Now.ToString(CultureInfo.CurrentCulture));
                 sb.AppendLine();
@@ -140,9 +205,12 @@ namespace playgroundv6
                 //        "Грешка при комуникация с POS устройството !");
                 //});
                 //return false;
-                Console.Read();
+
+                return result;
             }
+
         }
+    }
     }
 
     internal class PaymentSignatureCallback : IEmvPaymentSignatureCallback
@@ -191,5 +259,5 @@ namespace playgroundv6
             });
         }
     }
-}
+
 
